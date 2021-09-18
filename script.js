a35884d5b01063937ebcdf9045b5afb3c2d2228d
Gulnaz <88134441+GulnazSharshenalieva@users.noreply.github.com>
2021-09-18 14:19:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// TASK 1
+// let userName = prompt("What is yor name?");
+// document.write( "Hello," + " " + userName + "!");
+
+// TASK 2
+// let userFavTeam = prompt("What is your fav football team?");
+// document.write(userFavTeam.toUpperCase() + " " + "is champ!");
+
+// TASK 3
+// let userCardNumber = prompt("Please enter your card number");
+// document.write("The last 4 digits of your card:" + " " + userCardNumber.slice(8, 12));
+
+// TASK 3/1 Kasymov Azamat's solution
+// let creditCardNumber = prompt("Please enter your card number:");
+// let ccLength = creditCardNumber.length;
+// document.write(`The last 4 digits of your card: ${creditCardNumber.slice(ccLength-4, ccLength)}`);
+
+// TASK 4
+// let myString = "iVan";
+// let result = myString.charAt(0).toUpperCase() + myString.charAt(1).toLowerCase() + myString.slice(2);
+// document.write(result);
+
+// TASK 5
+// let myString = "$120";
+// document.write(myString.slice(1));
+
+// TASK 6
+// let userName = prompt("Hello, I'm a computer, and what is your name?");
+
+// let userAge = prompt("Nice to meet you, Nick, how old are you?");
+
+
+// let computerAnswer = prompt("Wow! 16!");
+
+// TASK 7
+// let userCreditCardNumber = prompt("Please enter your card number");
+// document.write("Your credit card number: **********" + userCreditCardNumber.slice(8, 12));
+
+
+// TASK 7/1
+// let userCreditCardNumber = prompt("Please enter your card number");
+// let ccLength = userCreditCardNumber.length;
+// document.write(`Your credit card number:**********${userCreditCardNumber.slice(ccLength-4, ccLength)}`);
